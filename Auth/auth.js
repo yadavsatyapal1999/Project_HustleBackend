5fda6c9570644e696147fd3c7661bd912b8019bd
Satyapal Yadav <114231518+yadavsatyapal1999@users.noreply.github.com>
2024-03-18 22:01:38 +0530

    Update auth.js

--- Auth/auth.js
+++ Auth/auth.js
@@ -6,7 +6,7 @@ const auth = (req,res,next)=>{
    
 try{
     const token = req.headers.authorization;
-   // console.log(token)
+ console.log(token)
 //console.log("try")
 
 jwt.verify(token,process.env.Secretkey);
